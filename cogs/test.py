55905ab64e4e5e64c7675b40a0942eb8c7a383bc
Sycephoa <fr2202fr@web.de>
2022-12-30 21:34:26 +0100

    .

--- cogs/test.py
+++ cogs/test.py
@@ -14,14 +14,12 @@ sys.path.insert(0, parentdir)
 from config import *
 import functions as f
 
-@app_commands.default_permissions(administrator=True)
 class Test(commands.GroupCog):
     
     def __init__(self,client):
         self.client = client
 
     @app_commands.command(name="test",description="test")
-    @app_commands.checks.has_permissions(administrator=True)
     async def setdebugchannel(self, inter: discord.Interaction):
         await inter.response.send_message(f'This works.',ephemeral=False)
         
